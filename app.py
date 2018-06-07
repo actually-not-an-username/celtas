6b47192c38239fc0a6b1adf768b48229f0bdfb27
JJimenez94 <JJimenez.9406@gmail.com>
2018-06-07 14:04:54 -0500

    Added creation of user, functional reports non functional

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ from flask import flash, Flask, render_template, request, redirect, url_for
 from flask_login import current_user, login_user, login_required, LoginManager, logout_user, UserMixin
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from flask_uploads import UploadSet, configure_uploads, send_from_directory
+from flask_uploads import UploadSet, configure_uploads, send_from_directory, IMAGES
+from pandas import DataFrame
 from sqlalchemy import func
+from sqlalchemy.orm import load_only
 from view.FormClasses import UserForm
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.urls import url_parse
@@ -19,8 +21,8 @@ login = LoginManager(app)
 login.login_view = 'login'
 login.login_message = 'Por favor inicie sesión para acceder a esta página'
 
-datasets = UploadSet("datasets", ("txt", "csv", "xls", "xlsx"))
-configure_uploads(app, datasets)
+photos = UploadSet("fotos", IMAGES)
+configure_uploads(app, photos)
 
 
 class Grupo(database.Model):
@@ -45,21 +47,18 @@ class Usuario(UserMixin, database.Model):
     tipodocumento = database.Column(database.Integer)
     eps = database.Column(database.String(250))
     niveleducativo = database.Column(database.Integer)
-    localidad = database.Column(database.Integer)
     telefono = database.Column(database.String(30))
     direccion = database.Column(database.String(30))
     institucioneducativa = database.Column(database.String(30))
     fechacreacion = database.Column(
         database.DateTime, index=True, default=datetime.utcnow)
-    activo = database.Column(database.Boolean)
-    estadoregistro = database.Column(database.Integer)
     rol = database.Column(database.Integer)
     idgrupo = database.Column(
         database.Integer, database.ForeignKey('grupo.id'))
     email = database.Column(database.String(250), index=True, unique=True)
     clave = database.Column(database.String(250), unique=True)
-    acudientes = database.relationship(
-        'Relativos', backref='acudientes', lazy='dynamic')
+    '''acudientes = database.relationship(
+        'Relativos', backref='acudientes', lazy='dynamic')'''
 
     def __repr__(self):
         return '<Usuario {} {}>'.format(self.nombre, self.apellido)
@@ -97,6 +96,68 @@ class Relativos(database.Model):
 
     def __repr__(self):
         return '<Acudiente {} {}>'.format(self.nombre, self.apellido)
+
+
+def clearFiles(filename, path):
+    filepath = os.path.join(path, filename)
+    if os.path.exists(filepath):
+        os.remove(filepath)
+
+
+def getFileExt(filename):
+    index = filename.rfind('.')
+    if (index != -1):
+        return filename[index:]
+    return None
+
+
+def changeName(currentFileName, id):
+    ext = getFileExt(currentFileName)
+    if ext != None:
+        return str(id) + ext
+    return None
+
+
+def findDocTypes(id):
+    doc_types = {
+        1: 'Cédula de ciudadanía',
+        2: 'Tarjeta de identidad',
+        3: 'Registro Civil'
+    }
+    return doc_types.get(id, "Tipo de identificación inválido")
+
+
+def findEducationLevels(id):
+    education_levels = {
+        1: 'Primero',
+        2: 'Segundo',
+        3: 'Tercero',
+        4: 'Cuarto',
+        5: 'Quinto',
+        6: 'Sexto',
+        7: 'Septimo',
+        8: 'Octavo',
+        9: 'Noveno',
+        10: 'Décimo',
+        11: 'Once',
+        12: 'Técnico',
+        13: 'Universitario'
+    }
+    return education_levels.get(id, "Nivel educativo inválido")
+
+
+def findRoles(id):
+    role_types = {
+        1: 'Administrador',
+        2: 'Colaborador',
+        3: 'Estudiante'
+    }
+    return role_types.get(id, "Rol inválido")
+
+
+def findGroups(id):
+    group = Grupo.query.get(int(id)).first()
+    return group.descripcion
 
 
 @login.user_loader
@@ -145,7 +206,7 @@ def logout():
 def user_management():
     page = request.args.get('page', default=1, type=int)
     users = Usuario.query.order_by(Usuario.apellido).paginate(
-        page, app.config['ROWS_PER_PAGE'], False)    
+        page, app.config['ROWS_PER_PAGE'], False)
     if users.has_next:
         next_url = url_for('user_management', page=users.next_num)
     else:
@@ -160,13 +221,81 @@ def user_management():
 @app.route("/create_user", methods=['GET', 'POST'])
 @login_required
 def create_user():
-    return render_template("layout.html")
+    form = UserForm()
+    form.group_id.choices = [(g.id, g.descripcion)
+                             for g in Grupo.query.order_by('descripcion')]
+    if form.validate_on_submit():
+        current_user = Usuario()
+        if form.birthdate.data is not None:
+            current_user.fechanacimiento = form.birthdate.data
+        if form.firstname.data is not None:
+            current_user.nombre = form.firstname.data
+        if form.surname.data is not None:
+            current_user.apellido = form.surname.data
+        if form.email.data is not None:
+            current_user.email = form.email.data
+        if form.password.data is not None:
+            current_user.set_password(form.password.data)
+        if form.blood_group.data is not None:
+            current_user.gruposanguineo = form.blood_group.data
+        if form.rh_factor.data is not None:
+            current_user.factorrh = form.rh_factor.data
+        if form.document_type.data is not None:
+            current_user.tipodocumento = form.document_type.data
+        if form.document_number.data is not None:
+            current_user.documento = form.document_number.data
+        if form.role_type.data is not None:
+            current_user.rol = form.role_type.data
+        if form.eps.data is not None:
+            current_user.eps = form.eps.data
+        if form.education_level.data is not None:
+            current_user.niveleducativo = form.education_level.data
+        if form.group_id.data is not None:
+            current_user.idgrupo = form.group_id.data
+        if form.height.data is not None:
+            current_user.estatura = form.height.data
+        if form.weight.data is not None:
+            current_user.peso = form.weight.data
+        if form.educative_plantel.data is not None:
+            current_user.institucioneducativa = form.educative_plantel.data
+        if form.address.data is not None:
+            current_user.direccion = form.address.data
+        if form.phone.data is not None:
+            current_user.telefono = form.phone.data
+        print(current_user)
+        '''uploadFile = request.picture.data
+        if uploadFile is not None:
+            newFileName = changeName(
+                uploadFile.filename, current_user.documento)
+            print('About to load picture :)' + newFileName)
+            if newFileName != None:
+                ext = getFileExt(newFileName)
+                path = os.path.join(
+                    app.config['UPLOADS_DEFAULT_DEST'], "photos/")
+                clearFiles(newFileName, path)
+                uploadFile.filename = newFileName
+                datasets.save(uploadFile)
+                flash('Foto del estudiante: ' + newFileName + ' cargada correctamente')'''
+        database.session.add(current_user)
+        database.session.commit()
+        return redirect(url_for('user_management'))
+    return render_template("user.html", form=form)
 
 
 @app.route("/get_user/<profile_id>")
 @login_required
-def query_user():    
+def query_user():
     return render_template("users.html", users=users.items, next_url=next_url, prev_url=prev_url)
+
+
+@app.route("/export_users")
+@login_required
+def get_report():
+    users = Usuario.query.with_entities(Usuario.nombre, Usuario.apellido, Usuario.fechanacimiento, Usuario.gruposanguineo, Usuario.factorrh, Usuario.peso, Usuario.estatura, Usuario.documento,
+                                        Usuario.tipodocumento, Usuario.eps, Usuario.niveleducativo, Usuario.telefono, Usuario.direccion, Usuario.institucioneducativa, Usuario.fechacreacion, Usuario.rol, Usuario.idgrupo, Usuario.email).all()
+    for user in users:
+        print((zip(user.keys(), user.values())))
+    return redirect('/uploads/results/listado_usuarios.xlsx')
 
 
 @app.route("/update_user/<profile_id>", methods=['GET', 'POST'])
@@ -184,7 +313,9 @@ def asistencia():
 @app.route("/test")
 def test():
     form = UserForm()
-    return render_template("user.html", form = form)
+    form.group_id.choices = [(g.id, g.descripcion)
+                             for g in Grupo.query.order_by('descripcion')]
+    return render_template("user.html", form=form)
 
 
 @app.route('/uploads/<path:filename>', methods=['GET', 'POST'])
--- view/FormClasses.py
+++ view/FormClasses.py
@@ -1,20 +1,41 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileAllowed, FileField, FileRequired
+from wtforms import StringField, PasswordField, BooleanField, SelectField, IntegerField
 from wtforms.fields.html5 import DateField
-from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import DataRequired, Email, ValidationError
+from wtforms.validators import DataRequired, Email, ValidationError, Optional
 
 
 class UserForm(FlaskForm):
     birthdate = DateField('Fecha de nacimiento', format='%Y-%m-%d')
-    picture = FileField('Seleccione la fotografía a cargar: ', validators=[FileAllowed(
-        ["sav"], "solamente se permiten archivos entregados por la aplicación")])
+    '''picture = FileField('Seleccione la foto: ', validators=[FileRequired(
+        message="Archivo requerido"), FileAllowed(
+        ['.jpg', '.jpe', '.jpeg', '.png', '.gif', '.svg', '.bmp'], "solamente se permiten fotos")])'''
     firstname = StringField('Nombres', validators=[DataRequired()])
     surname = StringField('Apellidos', validators=[DataRequired()])
-    email = StringField('Email', validators=[Email()])
-    password = PasswordField('Contraseña')        
-
-    def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user is not None:
-            raise ValidationError('La dirección de correo ya se encuntra en uso.')
+    password = PasswordField('Contraseña')
+    blood_group = SelectField(
+        'Grupo Sanguineo', choices=[('A', 'A'), ('B', 'B'), ('AB', 'AB'), ('O', 'O')], validators=[DataRequired()]
+    )
+    rh_factor = SelectField(
+        'Factor RH', choices=[('+', '+'), ('-', '-')], validators=[DataRequired()]
+    )
+    document_type = SelectField(
+        'Tipo de documento', choices=[('1', 'Cédula de ciudadanía'), ('2', 'Tarjeta de identidad'), ('3', 'Registro Civil')], validators=[DataRequired()]
+    )
+    document_number = StringField(
+        'Número de documento', validators=[DataRequired()])
+    role_type = SelectField(
+        'Rol', choices=[('1', 'Administrador'), ('2', 'Colaborador'), ('3', 'Estudiante')], validators=[DataRequired()]
+    )
+    eps = StringField('E.P.S', validators=[DataRequired()])
+    education_level = SelectField(
+        'Grado de escolaridad', choices=[('1', 'Primero'), ('2', 'Segundo'), ('3', 'Tercero'), ('4', 'Cuarto'), ('5', 'Quinto'), ('6', 'Sexto'), ('7', 'Séptimo'), ('8', 'Octavo'), ('9', 'Noveno'), ('10', 'Décimo'), ('11', 'Once'), ('12', 'Técnico'), ('13', 'Univesitario')], validators=[DataRequired()]
+    )
+    group_id = SelectField(u'Grupo', coerce=int)
+    height = IntegerField('Estatura en centimetros',
+                          validators=[DataRequired()])
+    weight = IntegerField('Peso en kilogramos', validators=[DataRequired()])
+    educative_plantel = StringField('Institución educativa')
+    address = StringField('Dirección', validators=[DataRequired()])
+    phone = StringField('Telefono', validators=[DataRequired()])
+    email = StringField('Email', validators=[Optional(), Email()])
