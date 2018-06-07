4bf44082da8ad0a3f4d6532a21ab92ceb46e1e24
JJimenez94 <JJimenez.9406@gmail.com>
2018-06-07 01:04:05 -0500

    rebased all the stuff

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from datetime import timedelta
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    UPLOADS_DEFAULT_DEST = "uploads"
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:dracula2000@localhost:5432/postgres'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    REMEMBER_COOKIE_DURATION = os.environ.get('REMEMBER_COOKIE_DURATION') or timedelta(seconds = 60)
+    ROWS_PER_PAGE = 20
